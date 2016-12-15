731d8a6144a00b77445a576a659d3d7858014919
godfern <godfern13@gmail.com>
2016-12-16 04:08:03 +0530

    Bargain api partially done

--- app.js
+++ app.js
@@ -5,26 +5,75 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mysql      = require('mysql');
+var rest = require("./REST.js");
+var md5 = require('MD5');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//var index = require('./routes/index');
+//var users = require('./routes/users');
 
 var app = express();
 
-var connection = mysql.createConnection({
-  host     : 'srm-db1.stage.ch.flipkart.com',
-  user     : 'root',
-  password : '',
-  database : 'bbt'
-});
+function REST(){
+  var self = this;
+  self.connectMysql();
+};
 
-connection.connect(function(err){
-  if(err){
-    console.log('Error connecting to Db');
-    return;
-  }
-  console.log('Connection established');
-});
+REST.prototype.connectMysql = function() {
+  var self = this;
+  var pool      =    mysql.createPool({
+    connectionLimit : 100,
+    host     : 'srm-db1.stage.ch.flipkart.com',
+    user     : 'root',
+    password : '',
+    database : 'bbt',
+    debug    :  false
+  });
+  pool.getConnection(function(err,connection){
+    if(err) {
+      self.stop(err);
+    } else {
+      self.configureExpress(connection);
+    }
+  });
+}
+
+REST.prototype.configureExpress = function(connection) {
+  var self = this;
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  var router = express.Router();
+  app.use('/api', router);
+  var rest_router = new rest(router,connection,md5);
+  self.startServer();
+}
+
+REST.prototype.startServer = function() {
+  app.listen(3000,function(){
+    console.log("All right ! I am alive at Port 3000.");
+  });
+}
+
+REST.prototype.stop = function(err) {
+  console.log("ISSUE WITH MYSQL n" + err);
+  process.exit(1);
+}
+
+new REST();
+
+//var connection = mysql.createConnection({
+//  host     : 'srm-db1.stage.ch.flipkart.com',
+//  user     : 'root',
+//  password : '',
+//  database : 'bbt'
+//});
+//
+//connection.connect(function(err){
+//  if(err){
+//    console.log('Error connecting to Db');
+//    return;
+//  }
+//  console.log('Connection established');
+//});
 
 //connection.end(function(err) {
 //  // The connection is terminated gracefully
@@ -33,47 +82,83 @@ connection.connect(function(err){
 //});
 
 
-connection.query('SELECT * FROM products',function(err,rows){
-  if(err) throw err;
+//connection.query('SELECT * FROM products',function(err,rows){
+//  if(err) throw err;
+//
+//  console.log('Data received from Db:\n');
+//  console.log(rows);
+//});
 
-  console.log('Data received from Db:\n');
-  console.log(rows);
-});
+//connection.end();
 
-connection.end();
+//app.get('/products',function(req,res){
+//  var data = {
+//    "error":1,
+//    "Books":""
+//  };
+//
+//  connection.query("SELECT * from products where FSN=",function(err, rows, fields){
+//    if(rows.length != 0){
+//      data["error"] = 0;
+//      data["Products"] = rows;
+//      res.json(data);
+//    }else{
+//      data["products"] = 'No products Found..';
+//      res.json(data);
+//    }
+//  });
+//});
+
+//BBT.prototype.getProductDetails= function(req,res) {
+  //router.get("/",function(req,res){
+  //    res.json({"Message" : "Hello World !"});
+  //})
+  //var query = "SELECT * FROM ?? WHERE ??=?";
+  ////var query = "INSERT INTO ??(??,??) VALUES (?,?)";
+  ////var table = ["user_login","user_email","user_password",req.body.email,md5(req.body.password)];
+  //var table = ["products","FSN",req.params.fsn];
+  //query = mysql.format(query,table);
+  //connection.query(query,function(err,rows){
+  //    if(err) {
+  //        res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+  //    } else {
+  //        res.json({"Error" : false, "Message" : "Products fetched !"});
+  //    }
+  //});
+//}
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+//app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+//
+//app.use('/', index);
+//app.use('/users', users);
+//
+//// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//  var err = new Error('Not Found');
+//  err.status = 404;
+//  next(err);
+//});
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//app.use(function(err, req, res, next) {
+//  // set locals, only providing error in development
+//  res.locals.message = err.message;
+//  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//  // render the error page
+//  res.status(err.status || 500);
+//  res.render('error');
+//});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+//module.exports = app;
--- rest.js
+++ rest.js
@@ -1,15 +1,91 @@
 /**
  * Created by godfrey.f on 16/12/16.
  */
+var mysql = require("mysql");
 function REST_ROUTER(router,connection,md5) {
     var self = this;
     self.handleRoutes(router,connection,md5);
 }
-
+//
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     router.get("/",function(req,res){
-        res.json({"Message" : "Hello World !"});
+        var productQuery = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["products","FSN",req.query.fsn];
+        var productQuery = mysql.format(productQuery,table);
+        var productDetailTry = {};
+        connection.query(productQuery,function(err,rows){
+            productDetailTry = rows;
+            console.log(JSON.stringify(rows));
+        });
+
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["fsn-user","FSN",req.query.fsn];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                if(rows[0]){
+                    var tryCount = parseInt(rows[0].try)+1;
+                    var query = "UPDATE  ?? SET ?? = ? WHERE ?? = ? AND ?? = ?";
+                    var table = ["fsn-user","try",tryCount,"FSN",req.query.fsn,"userId",req.query.userId];
+                    query = mysql.format(query,table);
+                    connection.query(query,function(err,rows){
+                        if(err) {
+                            console.log(err);
+                            res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+                        } else {
+                            res.json({"Error" : false, "Message" : "fsn-user added !"});
+                        }
+                    });
+                }else{
+                    var tryCount = 1;
+                    var query = "INSERT INTO ??(??,??,??) VALUES (?,?,?)";
+                    var table = ["fsn-user","FSN","userId","try",req.query.fsn,req.query.userId,tryCount];
+                    query = mysql.format(query,table);
+                    connection.query(query,function(err,rows){
+                        if(err) {
+                            console.log(err);
+                            res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+                        } else {
+                            res.json({"Error" : false, "Message" : "fsn-user added !"});
+                        }
+                    });
+                }
+
+                productDetailTry['try'] = tryCount;
+
+                console.log(JSON.stringify(productDetailTry));
+            }
+        });
     })
 }
 
 module.exports = REST_ROUTER;
+
+
+//function BBT(router,connection,md5) {
+//    var self = this;
+//    self.handleRoutes(router,connection,md5);
+//}
+
+//BBT.prototype.getProductDetails= function(req,res) {
+    //router.get("/",function(req,res){
+    //    res.json({"Message" : "Hello World !"});
+    //})
+
+    //var query = "INSERT INTO ??(??,??) VALUES (?,?)";
+    //var table = ["user_login","user_email","user_password",req.body.email,md5(req.body.password)];
+    //query = mysql.format(query,table);
+    //connection.query(query,function(err,rows){
+    //    if(err) {
+    //        res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+    //    } else {
+    //        res.json({"Error" : false, "Message" : "User Added !"});
+    //    }
+    //});
+
+
+//}
+
+//module.exports = BBT;
