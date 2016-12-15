625d4000c7e868336998ccdfdf6b02a6fbfebcc8
rakesh.katti <rakesh.katti@flipkart.com>
2016-12-16 04:16:46 +0530

    wip

--- app.js
+++ app.js
@@ -76,4 +76,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000, function () {
+  console.log('BBT app listening on port 3000!')
+})
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,70 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
+
+router.get('/product/getProduct', function (req, res) {
+    // var fsn =
+});
+
+router.post('/product/bargain', function (req, res) {
+    var bargain_price = req.body.bargain_price;
+    var fsn = req.body.fsn;
+
+    getProduct(fsn, bargain_price, function (bargain_price, result) {
+        var mrp = result.mrp;
+        var max_discount = result.max_discount;
+        var variable_discount = result.variable_discount;
+        var discount_limit = result.discount_limit;
+        var selling_price = mrp;
+        var bargain_status;
+
+        // TODO Update the tries
+        console.log(bargain_price);
+        if (bargain_price > variable_discount) {
+            bargain_status = false;
+            // should return tries alse
+        } else {
+            // Yay! we have him some discount
+            if (bargain_price <= variable_discount) {
+
+                if (bargain_price < max_discount) {
+                    var bargain_difference = max_discount - bargain_price;
+                    if (variable_discount + bargain_difference > discount_limit) {
+                        // TODO update discount to table vd = dl;
+                    }
+                }
+                if (bargain_price < max_discount) {
+                    bargain_status = true;
+                    selling_price = mrp - bargain_price;
+                }
+            }
+        }
+
+        res.json({
+            bargain_status: bargain_status,
+            selling_price
+        });
+    });
+
+    //check if he
+    // res.status(200).send({1:1});
+});
+
+
+router.post('/product/checkout', function (req, res) {
+
+})
+
+function getProduct(fsn, bp, callback) {
+    callback(bp, {
+        mrp: 5000,
+        max_discount: 500,
+        variable_discount: 600,
+        discount_limit: 800
+    });
+}
 
 module.exports = router;
